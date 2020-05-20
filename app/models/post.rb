3c14a75ad4b2272cec31b6fa6b59b50e8d0f23d1
aa1b02ab7492d696da6e8ac540ffa187ba623c88e15238d8d026f79bc5d39b3c <darrickpang@DESKTOP-02GI1R6.localdomain>
2020-05-19 21:25:02 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: :true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Nonfiction Fiction)}
+    validates :title, clickbait: true 
 end
+
